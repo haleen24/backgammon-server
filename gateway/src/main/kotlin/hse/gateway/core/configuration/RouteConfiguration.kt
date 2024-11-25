79e38078091a6354b3d02cb9455fa605541da686
haleen24 <ya.barrymor@yandex.ru>
2024-11-25 22:23:42 +0300

    readme для сервисов

--- gateway/src/main/kotlin/hse/gateway/core/configuration/RouteConfiguration.kt
+++ gateway/src/main/kotlin/hse/gateway/core/configuration/RouteConfiguration.kt
@@ -2,14 +2,15 @@ package hse.gateway.core.configuration
 
 import hse.gateway.core.service.JwtService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route
-import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http
+import org.springframework.cloud.gateway.route.Route
+import org.springframework.cloud.gateway.route.RouteLocator
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.servlet.function.RequestPredicates.path
-import org.springframework.web.servlet.function.RouterFunction
-import org.springframework.web.servlet.function.ServerRequest
-import org.springframework.web.servlet.function.ServerResponse
+import org.springframework.security.config.web.server.ServerHttpSecurity.http
+import org.springframework.web.reactive.function.server.RequestPredicates.path
+import org.springframework.web.reactive.function.server.RouterFunctions.route
+import java.util.function.Consumer
 
 @Configuration
 internal class RouteConfiguration {
@@ -18,26 +19,26 @@ internal class RouteConfiguration {
     private lateinit var jwtService: JwtService
 
     @Bean
-    fun menuRouteFunction(): RouterFunction<ServerResponse> {
-        return route("menu").route(path("menu/**"), http("http://localhost:81"))
-            .before(authHeaderRequestProcessor()).build()
-    }
-
-    @Bean
-    fun backGammonGameRouteFunction(): RouterFunction<ServerResponse> {
-        return route("backgammon-game").route(path("game/backgammon/**"), http("http://localhost:82"))
-            .before(authHeaderRequestProcessor()).build()
-    }
-
-    fun authHeaderRequestProcessor(): (ServerRequest) -> ServerRequest {
-        return { request ->
-            val token = request.cookies()["token"]?.firstOrNull()?.value
-            if (token == null) {
-                request
-            } else {
-                val userId = jwtService.extractUserId(token)
-                ServerRequest.from(request).header("auth-user", userId).build()
+    fun menuRouteFunction(routeLocatorBuilder: RouteLocatorBuilder): RouteLocator {
+        return routeLocatorBuilder.routes()
+            .route("menu") {
+                it.path("menu/**").customize { consumer -> authHeaderRequestProcessor(consumer) }
+                    .uri("http://localhost:81")
             }
+            .route("backgammon-game") {
+                it.path("game/backgammon/**").customize { consumer -> authHeaderRequestProcessor(consumer) }
+                    .uri("http://localhost:82")
+            }
+            .build()
+    }
+
+    fun authHeaderRequestProcessor(builder: Route.AsyncBuilder) {
+        builder.filter { exchange, chain ->
+            val token = exchange.request.cookies["token"]?.firstOrNull()?.value ?: return@filter chain.filter(exchange)
+            val userId = jwtService.extractUserId(token)
+
+            exchange.request.mutate().header("auth-user", userId)
+            chain.filter(exchange)
         }
     }
 }
--- gateway/src/main/kotlin/hse/gateway/core/service/UserService.kt
+++ gateway/src/main/kotlin/hse/gateway/core/service/UserService.kt
@@ -1,29 +1,29 @@
-package hse.gateway.core.service
-
-import hse.gateway.core.entity.User
-import hse.gateway.core.repository.UserRepository
-import org.springframework.security.crypto.password.PasswordEncoder
-import org.springframework.stereotype.Service
-
-@Service
-class UserService(
-    val userRepository: UserRepository,
-    val passwordEncoder: PasswordEncoder,
-) {
-    fun createUser(user: User) {
-        userRepository.save(user.copy(password = passwordEncoder.encode(user.password)))
-    }
-
-    fun findUser(username: String): User? {
-        return userRepository.findByUsername(username)
-    }
-
-    fun authenticate(login: String, password: String): User? {
-        val user = findUser(login) ?: return null
-        return if (passwordEncoder.matches(password, user.password)) {
-            user
-        } else
-            null
-
-    }
-}
+//package hse.gateway.core.service
+//
+//import hse.gateway.core.entity.User
+//import hse.gateway.core.repository.UserRepository
+//import org.springframework.security.crypto.password.PasswordEncoder
+//import org.springframework.stereotype.Service
+//
+//@Service
+//class UserService(
+//    val userRepository: UserRepository,
+////    val passwordEncoder: PasswordEncoder,
+//) {
+//    fun createUser(user: User) {
+//        userRepository.save(user.copy(password = passwordEncoder.encode(user.password)))
+//    }
+//
+//    fun findUser(username: String): User? {
+//        return userRepository.findByUsername(username)
+//    }
+//
+//    fun authenticate(login: String, password: String): User? {
+//        val user = findUser(login) ?: return null
+//        return if (passwordEncoder.matches(password, user.password)) {
+//            user
+//        } else
+//            null
+//
+//    }
+//}
