3fb2079fd08654eefe34d47817f03f50c99e5e52
haleen <ya.barrymor@yandex.ru>
2024-11-26 15:15:56 +0300

    Revert "api gateway на реакт"
    This reverts commit b963ba0a659f1d3ba6b619db07a322d30a5dc7fd.

--- gateway/src/main/kotlin/hse/gateway/core/configuration/SecurityConfig.kt
+++ gateway/src/main/kotlin/hse/gateway/core/configuration/SecurityConfig.kt
@@ -1,54 +1,40 @@
-package hse.gateway.core.configuration
-
-//import hse.gateway.core.configuration.filter.JwtAuthFilter
-import hse.gateway.core.configuration.filter.JwtAuthFilter
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Lazy
-import org.springframework.security.authentication.AuthenticationProvider
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
-import org.springframework.security.config.http.SessionCreationPolicy
-import org.springframework.security.config.web.server.SecurityWebFiltersOrder
-import org.springframework.security.config.web.server.ServerHttpSecurity
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.crypto.password.PasswordEncoder
-import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
-import org.springframework.security.web.server.SecurityWebFilterChain
-
-@Configuration
-@EnableWebFluxSecurity
-class SecurityConfig {
-
-
-    @Lazy
-    @Autowired
-    private lateinit var jwtAuthFilter: JwtAuthFilter
-
-//    @Autowired
-//    private lateinit var gatewayHeaderFilter: GatewayHeaderFilter
-
-
-    @Bean
-    fun securityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
-        return http
-            .csrf { it.disable() }
-            .authorizeExchange {
-                it.pathMatchers("/create/user", "/login").permitAll()
-                it.anyExchange().authenticated()
-            }
-            .sessionManagement { SessionCreationPolicy.STATELESS }
-            .addFilterAt(jwtAuthFilter, SecurityWebFiltersOrder.HTTP_BASIC)
-            .build()
-
+//package hse.gateway.core.configuration
+//
+////import hse.gateway.core.configuration.filter.JwtAuthFilter
+//import org.springframework.beans.factory.annotation.Autowired
+//import org.springframework.context.annotation.Bean
+//import org.springframework.context.annotation.Configuration
+//import org.springframework.context.annotation.Lazy
+//import org.springframework.security.authentication.AuthenticationProvider
+//import org.springframework.security.authentication.dao.DaoAuthenticationProvider
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+//import org.springframework.security.config.http.SessionCreationPolicy
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+//import org.springframework.security.crypto.password.PasswordEncoder
+//import org.springframework.security.web.SecurityFilterChain
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+//
+//
+//@Configuration
+//@EnableWebSecurity
+//class SecurityConfig {
+//
+//
+////    @Lazy
+////    @Autowired
+////    private lateinit var jwtAuthFilter: JwtAuthFilter
+//
+////    @Autowired
+////    private lateinit var gatewayHeaderFilter: GatewayHeaderFilter
+//
+//
+//    @Bean
+//    fun securityFilterChain(http: HttpSecurity, authenticationProvider: AuthenticationProvider): SecurityFilterChain {
 //        return http
 //            .csrf { it.disable() }
 ////            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter::class.java)
-//            .addFilterAfter(gatewayHeaderFilter, jwtAuthFilter::class.java)
+////            .addFilterAfter(gatewayHeaderFilter, jwtAuthFilter::class.java)
 //            .authenticationProvider(authenticationProvider)
 //            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
 //            .authorizeHttpRequests {
@@ -58,21 +44,21 @@ class SecurityConfig {
 //            }
 //            .formLogin { it.permitAll() }
 //            .build()
-    }
-
-    @Bean
-    fun authenticationProvider(
-        userDetailService: CustomUserDetailService,
-        passwordEncoder: PasswordEncoder
-    ): AuthenticationProvider {
-        val provider = DaoAuthenticationProvider()
-        provider.setUserDetailsService(userDetailService)
-        provider.setPasswordEncoder(passwordEncoder)
-        return provider
-    }
-
-    @Bean
-    fun passwordEncoder(): PasswordEncoder {
-        return BCryptPasswordEncoder()
-    }
-}
+//    }
+//
+//    @Bean
+//    fun authenticationProvider(
+//        userDetailService: CustomUserDetailService,
+//        passwordEncoder: PasswordEncoder
+//    ): AuthenticationProvider {
+//        val provider = DaoAuthenticationProvider()
+//        provider.setUserDetailsService(userDetailService)
+//        provider.setPasswordEncoder(passwordEncoder)
+//        return provider
+//    }
+//
+//    @Bean
+//    fun passwordEncoder(): PasswordEncoder {
+//        return BCryptPasswordEncoder()
+//    }
+//}
--- gateway/src/main/kotlin/hse/gateway/core/configuration/filter/JwtAuthFilter.kt
+++ gateway/src/main/kotlin/hse/gateway/core/configuration/filter/JwtAuthFilter.kt
@@ -1,33 +1,39 @@
-package hse.gateway.core.configuration.filter
-
-import hse.gateway.core.service.JwtService
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
-import org.springframework.security.core.context.ReactiveSecurityContextHolder
-import org.springframework.security.core.userdetails.UserDetailsService
-import org.springframework.stereotype.Component
-import org.springframework.web.server.ServerWebExchange
-import org.springframework.web.server.WebFilter
-import org.springframework.web.server.WebFilterChain
-import reactor.core.publisher.Mono
-
-@Component
-class JwtAuthFilter(
-    private val jwtService: JwtService,
-    private val userDetailsService: UserDetailsService
-): WebFilter {
-
-    override fun filter(exchange: ServerWebExchange, chain: WebFilterChain): Mono<Void> {
-        val token = exchange.request.cookies?.getFirst("token" )?.value ?: return chain.filter(exchange)
-
-        val username = jwtService.extractUserName(token)
-
-        val userDetails = userDetailsService.loadUserByUsername(username)
-
-        if (jwtService.validateToken(token, userDetails)) {
-            val authenticationToken = UsernamePasswordAuthenticationToken(userDetails, null, userDetails.authorities)
-            return chain.filter(exchange).contextWrite(ReactiveSecurityContextHolder.withAuthentication(authenticationToken))
-        }
-
-       return chain.filter(exchange)
-    }
-}
+//package hse.gateway.core.configuration.filter
+//
+//import hse.gateway.core.service.JwtService
+//import jakarta.servlet.FilterChain
+//import jakarta.servlet.http.HttpServletRequest
+//import jakarta.servlet.http.HttpServletResponse
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+//import org.springframework.security.core.context.SecurityContextHolder
+//import org.springframework.security.core.userdetails.UserDetailsService
+//import org.springframework.security.web.authentication.WebAuthenticationDetailsSource
+//import org.springframework.stereotype.Component
+//import org.springframework.web.filter.OncePerRequestFilter
+//
+//@Component
+//class JwtAuthFilter(
+//    private val jwtService: JwtService,
+//    private val userDetailsService: UserDetailsService
+//) : OncePerRequestFilter() {
+//    override fun doFilterInternal(
+//        request: HttpServletRequest,
+//        response: HttpServletResponse,
+//        filterChain: FilterChain
+//    ) {
+//        val token =
+//            request.cookies?.firstOrNull { it.name == "token" }?.value ?: return filterChain.doFilter(request, response)
+//
+//        val username = jwtService.extractUserName(token)
+//
+//        val userDetails = userDetailsService.loadUserByUsername(username)
+//
+//        if (jwtService.validateToken(token, userDetails)) {
+//            val authenticationToken = UsernamePasswordAuthenticationToken(userDetails, null, userDetails.authorities)
+//            authenticationToken.details = WebAuthenticationDetailsSource().buildDetails(request)
+//            SecurityContextHolder.getContext().authentication = authenticationToken
+//        }
+//
+//        filterChain.doFilter(request, response)
+//    }
+//}
