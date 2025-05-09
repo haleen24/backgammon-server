1975d6bcc5e2d94c88a3a5ef1e947312dd8d88ec
haleen24 <ya.barrymor@yandex.ru>
2025-05-09 15:37:24 +0300

    fix деплоя

--- deploy-scripts/deploy.sh
+++ deploy-scripts/deploy.sh
@@ -1,5 +1,7 @@
 kubectl delete deploy "$1"
 eval $(minikube docker-env)
-docker image rm "$1" --force
-docker build -t "$1" .
+sudo docker image rm "$1" --force
+sudo docker build -t "$1" .
+sudo docker tag "$1":latest localhost:5000/"$1":latest
+sudo docker push localhost:5000/"$1":latest
 kubectl apply -f "../k8s/$1".yaml
