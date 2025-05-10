beb5f01f10fb565f3caea31ec27a7873fe07553a
haleen24 <ya.barrymor@yandex.ru>
2025-05-10 14:15:24 +0300

    хотфикс изменения рейтинга

--- player-service/src/main/kotlin/hse/playerservice/controller/UserRatingController.kt
+++ player-service/src/main/kotlin/hse/playerservice/controller/UserRatingController.kt
@@ -2,6 +2,7 @@ package hse.playerservice.controller
 
 import hse.playerservice.service.UserRatingService
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import player.request.ChangeRatingRequest
@@ -12,7 +13,7 @@ class UserRatingController(
     val userRatingService: UserRatingService
 ) {
     @PostMapping
-    fun changeRating(changeRatingRequest: ChangeRatingRequest) {
+    fun changeRating(@RequestBody changeRatingRequest: ChangeRatingRequest) {
         userRatingService.changeRating(changeRatingRequest)
     }
 }
