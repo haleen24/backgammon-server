2c18ae9c4927b87707a6bf602fc725ebfd9f669b
haleen24 <ya.barrymor@yandex.ru>
2025-05-19 22:22:06 +0300

    фикс подсчета рейтинга

--- player-service/src/main/kotlin/hse/playerservice/PlayerServiceApplication.kt
+++ player-service/src/main/kotlin/hse/playerservice/PlayerServiceApplication.kt
@@ -2,8 +2,10 @@ package hse.playerservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.transaction.annotation.EnableTransactionManagement
 
 @SpringBootApplication
+@EnableTransactionManagement
 class PlayerServiceApplication
 
 fun main(args: Array<String>) {
