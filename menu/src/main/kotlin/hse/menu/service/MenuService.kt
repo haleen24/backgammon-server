43bd3bf48ca41375b85979e73827ab7329b004e9
haleen24 <ya.barrymor@yandex.ru>
2025-05-20 23:27:07 +0300

    menu-service фикс ответов на инвайты

--- menu/src/main/kotlin/hse/menu/service/MenuService.kt
+++ menu/src/main/kotlin/hse/menu/service/MenuService.kt
@@ -154,7 +154,7 @@ class MenuService(
 
     @Transactional
     fun answerOnInvite(userId: Long, invitedBy: Long, accept: Boolean): Long {
-        val game = gameService.findByPlayersAndStatus(userId, invitedBy, GameStatus.NOT_STARTED)
+        val game = gameService.findByPlayersAndStatus(invitedBy, userId, GameStatus.NOT_STARTED)
             ?: throw ResponseStatusException(HttpStatus.NOT_FOUND, "Invitation not found")
         if (!accept) {
             sseEmitterService.send(RejectInviteDto(userId), listOf(invitedBy))
